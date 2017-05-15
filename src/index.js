c7c9ae263e467d3a13cfbdbae29d124fd61845d3
Taha Saleh <taha.a.saleh@gmail.com>
2017-05-15 12:49:26 +0200

    Add delete for items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-angular
-  .module('app', []);
+var app = angular.module('todo-list', []);
+
+app.controller(
+  'todo-list-controller', function($scope, $http){
+    $scope.applicationName = 'Todo List';
+    $scope.allItems = []
+
+    $http.get('http://localhost:3000/items.json').then(
+      function(success){
+        $scope.allItems = success.data
+        console.log(success.data)
+      },
+      function(err){
+        console.log(err)
+      }  
+    );
+
+    $scope.addNewItem = function(){
+      var x = {
+        description: $scope.itemDescription
+      }
+      console.log(x)
+
+      $http.post('http://localhost:3000/items.json',{item: x}).then(
+        function(success){
+          $scope.itemDescrition = '';
+          $scope.allItems.push(success.data);
+        },
+        function(err){
+          console.log("eroooooor")
+        }
+      );
+    }
+
+    $scope.deleteItem = function(item) { 
+      $http.delete('http://localhost:3000/items/'+item.id+'.json' ).then(
+        function(success){
+          var index = $scope.allItems.indexOf(item);
+          $scope.allItems.splice(index, 1); 
+        }
+
+       );  
+    }
+
+
+  });
